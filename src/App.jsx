8cf24a5554eb860d5f55533df7361a9af1af2c40
Uttkarsh <rupareluttkarsh2309@gmail.com>
2025-03-30 15:53:20 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import OngoingDrives from "./pages/OngoingDrives";
 import UpcomingDrives from "./pages/UpcomingDrives";
 import Auth from "./pages/Auth";
 import TechDirectory from "./pages/TechDirectory";
-
+import EditProfile from "./pages/EditProfile";
 
 
 function App() {
-    return <TechDirectory />;
+    return <Auth/>;
 }
 
 export default App;
