203f1586bc1f8d7ebb397252a40dcd7ec417458f
rudra <rudrakhandelwal1212@gmail.com>
2025-04-21 13:17:59 +0530

    this is a prof commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    strictPort: true,
-  },
-});
+  plugins: [react()],
+})
